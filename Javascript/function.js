4f3ac384f62386e9da323ea845597b0b2bfebd45
your_github_username <pratikmirchapure46@gmail.com>
2021-12-31 16:01:33 +0530

    overlays

---
+++ Javascript/function.js
@@ -0,0 +1,18 @@
+/* function method
+function defination
+function call
+*/
+
+// function defination 
+
+// function fun1(){
+//     console.log('hi')
+//     console.log('Byee')
+
+// }
+
+// function call 
+// fun1()
+
+
+console.log(0.2+0.3)
