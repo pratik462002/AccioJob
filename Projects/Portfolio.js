b66f4efeb95e4c0a0eb72e454a6cad773ce7f856
your_github_username <pratikmirchapure46@gmail.com>
2022-01-21 21:29:06 +0530

    push

--- Projects/Portfolio.js
+++ Projects/Portfolio.js
@@ -1,35 +1,69 @@
-const companyname = document.querySelectorAll(".NEO");
+// const companyname = document.querySelectorAll(".NEO");
 
-companyname.forEach((highlight) => {
-  highlight.addEventListener("click", () => colour(highlight));
-});
+// //  companyname.forEach((highlight) => {
+// //    highlight.addEventListener("click", () => colour(highlight));
+// //  })
 
-function colour(highlight){
-    companyname.forEach((highlight)=>{
-        highlight.classList.remove('clicked')
-    })
-    highlight.addEventListener("click", () => active('allexperiences') );
+// function colour(highlight){
+//     companyname.forEach((highlight)=>{
+//         highlight.classList.remove('clicked')
+//     })
+//     highlight.classList.add('clicked')
+// }
+
+// function active(abc){
+//     highlight.classList.add('clicked')
+//     let x=document.getElementsByClassName(abc)
+//     for(let i=0 ;i<x.length;i++){
+//         var s = x[i].style;
+//       s.display = s.display === 'none' ? 'block' : 'none';
+//     }
+// }
+
+var divs = ["Section1", "Section2", "Section3", "Section4", "Section5"];
+var visibleId = null;
+function show(id) {
+  if (visibleId !== id) {
+    visibleId = id;
+  }
+  hide();
 }
-
-function active(abc){
-    highlight.classList.add('clicked')
-    let x=document.getElementsByClassName(abc)
-    for(let i=0 ;i<x.length;i++){
-        var s = x[i].style;
-      s.display = s.display === 'none' ? 'block' : 'none';
+function hide() {
+  var div, i, id;
+  for (i = 0; i < divs.length; i++) {
+    id = divs[i];
+    div = document.getElementById(id);
+    if (visibleId === id) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
     }
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
+var colourdivs = ["Section6", "Section7", "Section8", "Section9", "Section10"];
+var colourId = null;
+function colour(id2) {
+  if (colourId !== id2) {
+    colourId = id2;
+  }
+  remove();
+}
+function remove() {
+  var div, i, id2;
+  for (i = 0; i < colourdivs.length; i++) {
+    id2 = colourdivs[i];
+    divcolour = document.getElementById(id2);
+    if (colourId === id2) {
+      divcolour.style.backgroundColor = "#64ffda";
+      divcolour.style.color = "black";
+      console.log(id2);
+    } else {
+      divcolour.style.backgroundColor = "black";
+      divcolour.style.color = "white";
+    }
+  }
+}
 
 // function toggle_visibility(cl){
 //    var els = document.getElementsByClassName(cl);
@@ -51,5 +85,3 @@ function active(abc){
 //         highlight.classList.remove('clicked')
 //     }
 // }
-
-
