fd42b242ceb78bad221af84b17b10f03eddf7ace
your_github_username <pratikmirchapure46@gmail.com>
2022-02-11 23:37:16 +0530

    push

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,12 +1,35 @@
 const temp = document.querySelector(".temp");
 const feelsLike = document.querySelector(".feels-like");
-const icon = document.querySelector(".icon");
+const icon = document.querySelector(".icon img");
 const city = document.querySelector(".city");
 
 const input = document.querySelector(".input");
 const submit = document.querySelector(".submit");
 
 const currentStatus = document.querySelector(".status");
+
+let apiKey = "c2ee8017d562bb6b49e77d514f8e251c";
+
+window.addEventListener("load", function () {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+    });
+  }
+  const api1 = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+
+  fetch(api1)
+    .then((response1) => response1.json())
+    .then((result1) => console.log(result1));
+});
+
+
+
+
 
 submit.addEventListener("click", function () {
   if (input.value !== "") {
@@ -19,7 +42,6 @@ submit.addEventListener("click", function () {
 });
 
 function requestApi(city) {
-  let apiKey = "c2ee8017d562bb6b49e77d514f8e251c";
   let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   currentStatus.innerHTML = "Getting weather deatils....";
   fetch(api)
@@ -36,11 +58,12 @@ function weatherData(info) {
     const city2 = info.name;
     const country2 = info.sys.country;
 
-    const icon2 = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+    const icon2 = `https://openweathermap.org/img/wn/${info.weather[0]["icon"]}@2x.png`;
 
     const temp2 = info.main.temp;
     const { description, id } = info.weather[0];
 
+    icon.src = `${icon2}`;
     const { feels_like } = info.main;
 
     city.innerHTML = `${city2}/ ${country2}`;
